47a2d4be769265f4f94b1513aca398d640444ffb
vova <149654537+End3rmag@users.noreply.github.com>
2024-10-30 16:07:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System;
+using System.Reflection.Metadata;
+
+Console.WriteLine("Введите размер матрици: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Snake: ") ;
+int[,] matrix = new int[n, n];
+
+int start = 1;
+
+for (int k = 1 - n; k <= n - 1; k++)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int j = i - k;
+
+        if (j < 0 || j >= n)
+            continue;
+        if ((k % 2) != 0)
+        { 
+           matrix[i, n - 1 - j] = start++;
+        }
+        else
+        {
+            matrix[n - 1 - j, i] = start++;
+        } 
+
+    }
+    
+}
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+        
+       Console.Write(matrix[i, j] + "|");
+    Console.WriteLine();
+}
